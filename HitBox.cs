cbf1e0b22dbd466ce7fbdacdf9e5733040a366bb
Blukat <justi1jc@cmich.edu>
2017-03-05 19:18:22 -0500

    Removed old scripts

--- HitBox.cs
+++ HitBox.cs
@@ -1,45 +1,16 @@
-﻿/*
-*
-*        Author: James Justice
-*
-*        HitBox is meant to use in conjunction with an FPSCharacterController
-*        in order to record damage done to a particular npcpart.
-*
-*        ReceiveDamage() Sends the damage directly to the script at present.
-*
-*
-*
-*
-*
-*
+/*
+*     Author: James Justice
+*     A HitBox allows projectiles and other damaging items to direct their damage.
+*     To the parent Actor.
 */
 
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class OldHitBox : MonoBehaviour {
-   public bool foot;
-   public float safeFallDistance;
-	public NPC npc;
-	public void ReceiveDamage(int damage, GameObject weapon){
-	   if(npc != null){
-	      npc.ReceiveDamage(damage, weapon);
-	   }
-	}
-	void OnCollisionEnter(Collision col){  
-      if(foot && npc.IsFalling()){
-         npc.Land();
-         float start = npc.FallOrigin().y;
-         float end = npc.GetGameObject().transform.position.y;
-         float distanceFallen =  start - end;
-         if(start > end && distanceFallen > safeFallDistance){
-            ReceiveDamage((int)(distanceFallen-safeFallDistance)*5, gameObject);
-         }  
-      }
-	}
-	public void OnCollisionStay(Collision col){
-	   if(foot && npc.IsFalling()){
-	      npc.Land();
-	   }
-	}
+public class HitBox : MonoBehaviour {
+  public Actor body;
+  public void ReceiveDamage(int damage, GameObject weapon){
+    if(!body){ return; }
+    body.ReceiveDamage(damage, weapon);
+  }
 }
