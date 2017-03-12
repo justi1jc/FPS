ada8c995a7e5dc7f2e069014ad1ac140127a840f
Blukat <justi1jc@cmich.edu>
2017-03-12 15:31:37 -0400

    Broke up Think loop between different AI profiles.

--- AI.cs
+++ AI.cs
@@ -11,15 +11,24 @@ using System.Collections.Generic;
 
 
 public class AI: MonoBehaviour{
+  // AI profile
+  public const int PASSIVE = 0; // flee when possible
+  public const int HOSTILE = 1; // fight when possible
+  public const int NEUTRAL = 2; // fight selectively
+  
   bool paused;
   Actor host;
   float sightDistance = 20f;
-  
+  float accuracy = 1f;
+  public int profile; // What sort of AI is this?
+  List<Actor> sighted;
+  List<Vector3> lastKnownPos;
   
   /* Initial setup. */
   public void Begin(Actor _host){
     host = _host;
     paused = false;
+    sighted = new List<Actor>();
     StartCoroutine(Think());
   }
   
@@ -35,15 +44,69 @@ public class AI: MonoBehaviour{
   
   IEnumerator Think(){
     while(true){
-      Actor a = ScanForEnemy();
-      if(a){ print(a); }
+      if(!paused){
+        switch(profile){
+          case PASSIVE:
+          yield return StartCoroutine(ThinkPassive());
+          break;
+          case HOSTILE:
+          yield return StartCoroutine(ThinkHostile());
+          break;
+          case NEUTRAL:
+          break;
+        }
+      }
       yield return new WaitForSeconds(1f);
     }
   }
   
+  /* Decision logic for passive AI */
+  IEnumerator ThinkPassive(){
+    yield return new WaitForSeconds(100f);
+  }
+  
+  /* Decision logic for hostile AI */
+  IEnumerator ThinkHostile(){
+    if(paused){ yield break; }
+    UpdateEnemies();
+    for(int i = 0; i < sighted.Count; i++){
+      if(sighted[i]){
+        yield return StartCoroutine(Fight(sighted[i]));
+      }
+    }
+    yield return new WaitForSeconds(0f);
+  }
+  
+  /* Gather sighted hostiles */
+  void UpdateEnemies(){
+    Actor enemy = ScanForActor();
+    sighted = new List<Actor>();
+    lastKnownPos = new List<Vector3>();
+    if(enemy){
+      sighted.Add(enemy);
+      lastKnownPos.Add(enemy.body.transform.position);
+    }
+  }
+  
+  
+  /* Engages enemy in combat */
+  IEnumerator Fight(Actor enemy){
+    if(paused){ yield break;}
+    if(!CanSee(enemy.head)){ StartCoroutine(SearchFor(enemy)); yield break;}
+    yield return new WaitForSeconds(0f);
+  }
+  
+  IEnumerator SearchFor(Actor target){
+    yield return new WaitForSeconds(0f);
+  }
+  
+  /* Aim at the target */
+  IEnumerator AimAt(){
+    yield return new WaitForSeconds(0f);
+  }
   
   /* Returns an Actor in sight, or null.  */
-  Actor ScanForEnemy(){
+  Actor ScanForActor(){
     Vector3 center = host.body.transform.position;
     Vector3 halfExtents = new Vector3(10f, 10f, 1f);
     Vector3 direction = host.body.transform.forward;
