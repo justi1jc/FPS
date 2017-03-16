e5ed1aae9576b42b02f6b9028a4e39a88e4775b7
Blukat <justi1jc@cmich.edu>
2017-03-16 14:33:59 -0400

    Created skeleton for SpeechNode and SpeechTree.

--- SpeechTree.cs
+++ SpeechTree.cs
@@ -1,44 +1,51 @@
 /*
-*  The speech tree is responsible for storing the text found in
-*  one text file as a series of SpeechNodes. Each prompt has at
-*  most four responses. It is assumed the first node in the list is the root.
+  The speech tree is responsible for parsing a text file into a tree of
+  SpeechNode objects.
+
+
+  SPEECH NODE TEXT FILE FORMAT:
+  <ARGUMENTS>:[<ARGUMENT>:<VALUE>]
+
 */
 using System.Collections.Generic;
 
 public class SpeechTree{
-  public string file;
-  //List<SpeechNode> nodes;
-  public int root, active;
+  public string fileName;
+  public int activeNode;
+  public List<SpeechNode> nodes;
   
-  /* Constructor */
   public SpeechTree(){
-    root = active = 0;
+    
   }
   
-   
+  /* Returns active node. */
+  public SpeechNode ActiveNode(){
+    return nodes[activeNode];
+  }
+  
+  /* Selects an option and performs its actions. */
+  public void SelectOption(int option){
+    SpeechNode node = ActiveNode();
+    switch(node.actions[option]){
+      case SpeechNode.NONE:
+        break;
+      case SpeechNode.SHOW_NODE:
+        break;
+      case SpeechNode.HIDE_NODE:
+        break;
+      case SpeechNode.CHANGE_CHILD:
+        break;
+      case SpeechNode.CHANGE_ACTION:
+        break;
+    }
+  }
   
   
   /* Reads data in from file. */
   public void ParseFile(){
   }
-}
-
-/*
-private class SpeechNode(){
-  string parentOption, prompt;// 
-  int one, two, three, four;  // Child nodes
   
-  public SpeechNode(
-                   _parentOption,
-                   _prompt,
-                   _one = -1,
-                   _two = -1,
-                   _three = -1,
-                   _four = -1
-                   ){
-   this.
-
+  /* Creates a node.*/
+  public void ParseNode(){
   }
-
 }
-*/
