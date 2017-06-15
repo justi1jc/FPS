9c0f52a76b22a2ca8732e622b2f72b9e6ba341e9
Blukat <justi1jc@cmich.edu>
2017-06-15 19:00:10 -0400

    Fleshed out an inventory component.

--- Inventory.cs
+++ Inventory.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 [System.Serializable]
 public class Inventory{
   public List<Data> inv;
+  int slots; // Max number of slides.
   
   public Inventory(){
     inv = new List<Data>();
@@ -18,5 +19,60 @@ public class Inventory{
   
   public Inventory(List<Data> _inv){
     inv = _inv;
+  }
+  
+  /* Stores this item and returns the overflow if it can't fit */
+  public int Store(Data dat){
+    for(int i = 0; i < inv.Count; i++){ 
+      if(dat.stack == 0){ return 0; }
+      if(
+          inv[i] != null &&
+          inv[i].displayName == dat.displayName &&
+          inv[i].stack < inv[i].stackSize 
+      ){
+        dat.stack = StackItem(i, dat);
+      }
+    }
+    if(dat.stack == 0){ return 0; }
+    
+    for(int i = 0; i < inv.Count; i++){
+      if(inv[i] == null){
+        inv[i] = dat;
+        return 0;
+      }
+    }
+    return dat.stack;
+  }
+  
+  /* fills out selected slot and returns the remainder. */
+  public int StackItem(int slot, Data dat){
+    int sum = dat.stack + inv[slot].stack;
+    if(sum <= inv[slot].stackSize){
+      inv[slot].stack = sum;
+      return 0;
+    }
+    else{ return sum - inv[slot].stackSize; }
+  }
+  
+  /* Removes up to the quantity of items from a given slot, and returns it
+     or null. */
+  public Data Retrieve(int slot, int quantity = 1){
+    if(slot < 0 || slot >= inv.Count || inv[slot] == null){ return null; }
+    Data ret = inv[slot];
+    if(quantity < inv[slot].stack){ 
+      ret.stack = quantity;
+      inv[slot].stack -= quantity;
+    }
+    else{
+      inv[slot] = null; 
+    }
+    return ret;
+    
+  }
+  
+  /* Return the contents of a slot, or null */
+  public Data Peek(int slot){
+    if(slot < 0 || slot >= inv.Count || inv[slot] == null){ return null; }
+    return inv[slot];
   }
 }
